1844888a0074e7c9fc867ea08f162e635fa635b1
oguzhan <oguzhan.ornek@enqura.com>
2023-05-01 21:13:24 +0300

    api key ekledi

---
+++ app/src/main/java/com/alierkoc/filmlerv1/infra/Constants.kt
@@ -0,0 +1,5 @@
+package com.alierkoc.filmlerv1.infra
+
+object Constants {
+    const val apiKey = "1ae81a0b7ff5aa1b20644e098cf3cd41"
+}
--- app/src/main/java/com/alierkoc/filmlerv1/ui/activies/MainActivity.kt
+++ app/src/main/java/com/alierkoc/filmlerv1/ui/activies/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.alierkoc.filmlerv1
+package com.alierkoc.filmlerv1.ui.activies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alierkoc.filmlerv1.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
