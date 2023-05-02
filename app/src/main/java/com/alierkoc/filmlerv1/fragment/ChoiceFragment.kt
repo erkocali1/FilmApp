33159117e4a48073640755f0b94d0bc08bc44c92
erkocali1 <muhammedalierkoc92@gmail.com>
2023-05-03 00:30:16 +0300

    muzo started 2

---
+++ app/src/main/java/com/alierkoc/filmlerv1/fragment/ChoiceFragment.kt
@@ -0,0 +1,21 @@
+package com.alierkoc.filmlerv1.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.alierkoc.filmlerv1.R
+
+
+class ChoiceFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_choice, container, false)
+    }
+
+}
