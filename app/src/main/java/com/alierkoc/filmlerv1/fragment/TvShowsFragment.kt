689bdaff40662b51ffe0af80e846a971612011fd
erkocali1 <muhammedalierkoc92@gmail.com>
2023-05-03 01:02:22 +0300

    muzo started 3

--- app/src/main/java/com/alierkoc/filmlerv1/fragment/TvShowsFragment.kt
+++ app/src/main/java/com/alierkoc/filmlerv1/fragment/TvShowsFragment.kt
@@ -7,14 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 import com.alierkoc.filmlerv1.R
 
-class MovieFragment : Fragment() {
+
+class TvShowsFragment : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie, container, false)
+        return inflater.inflate(R.layout.fragment_tv_shows, container, false)
     }
 
 }
---
+++ app/src/main/java/com/alierkoc/filmlerv1/ui/activies/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.alierkoc.filmlerv1.ui.activies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alierkoc.filmlerv1.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
